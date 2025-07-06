2a6e7383bc7bcfb3e438921b3307fab8409d7130
MisaelPalomino <misael@misael-HP-Laptop-15-ef2xxx.(none)>
2025-07-06 16:50:55 -0500

    Implementacion getters y setters de Ejercicio

--- src/main/java/com/soft/gymapp/dominio/planesentrenamiento/Ejercicio.java
+++ src/main/java/com/soft/gymapp/dominio/planesentrenamiento/Ejercicio.java
@@ -16,7 +16,60 @@ public class Ejercicio {
     @ManyToOne
     private Rutina rutina;
 
+    public Ejercicio() {
+        // Constructor requerido por JPA
+    }
+
     public float calcularCaloriasQuemadas(int duracion) {
-        return 0;
+        return duracion;
+    }
+
+    // Getters y Setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getRepeticiones() {
+        return repeticiones;
+    }
+
+    public void setRepeticiones(int repeticiones) {
+        this.repeticiones = repeticiones;
+    }
+
+    public int getSeries() {
+        return series;
+    }
+
+    public void setSeries(int series) {
+        this.series = series;
+    }
+
+    public Rutina getRutina() {
+        return rutina;
+    }
+
+    public void setRutina(Rutina rutina) {
+        this.rutina = rutina;
     }
 }
