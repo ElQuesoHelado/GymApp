dd573a462d84c0df54f4d5a9a50f5b6d6bd6dfcf
Saelemaekers <gonzarzp@gmail.com>
2025-07-21 22:32:51 -0500

    Repo Usuario

--- src/main/java/com/soft/gymapp/dominio/usuarios/UsuarioRepositorio.java
+++ src/main/java/com/soft/gymapp/dominio/usuarios/UsuarioRepositorio.java
@@ -1,8 +1,14 @@
 package com.soft.gymapp.dominio.usuarios;
 
+import com.soft.gymapp.dominio.membresias.Membresia;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UsuarioRepositorio extends JpaRepository<Usuario, Integer> {
+
+    List<Membresia> findByCuentaUsuarioUsername(String nombre);
+
 //    void guardar(Usuario usuario);
 //
 //    void actualizar(Usuario usuario);
