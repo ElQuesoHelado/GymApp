a3c7e85f4d6319807d6211e7b483ec1188912ad4
Saelemaekers <gonzarzp@gmail.com>
2025-07-22 05:50:17 -0500

    Se agrego una función para membresías adeudas y canceladas

--- src/main/java/com/soft/gymapp/dominio/membresias/MembresiaRepositorio.java
+++ src/main/java/com/soft/gymapp/dominio/membresias/MembresiaRepositorio.java
@@ -12,3 +12,14 @@ public interface MembresiaRepositorio extends JpaRepository<Membresia, Integer> 
 
     List<Membresia> findByTipoDuracionDiasGreaterThanEqual(int diasMinimos);
 }
+
+//G
+package com.gymapp.repository;
+
+import com.gymapp.entity.Membresia;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface MembresiaRepository extends JpaRepository<Membresia, Long> {
+    List<Membresia> findByEstado(Membresia.EstadoMembresia estado);
+}
