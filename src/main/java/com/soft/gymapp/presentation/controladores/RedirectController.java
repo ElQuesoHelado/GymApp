000c9087c2eae06ba8d1b79eaccd2b7f4becec72
sh3ld0 <driverac@unsa.edu.pe>
2025-12-21 09:41:52 -0500

    refactor: Eliminar imports no usados en RedirectController. Fix #32

--- src/main/java/com/soft/gymapp/presentation/controladores/RedirectController.java
+++ src/main/java/com/soft/gymapp/presentation/controladores/RedirectController.java
@@ -2,7 +2,6 @@ package com.soft.gymapp.presentation.controladores;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
