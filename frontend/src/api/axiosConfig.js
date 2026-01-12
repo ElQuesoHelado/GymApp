4d83ce49a66fa3cce0c3f96127479ec34873c866
ElQuesoHelado <josealvacornejo1@gmail.com>
2026-01-12 17:09:10 -0500

    Determinar port backend desde docker

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_API_URL || 'http://backend:8080/api';
-//const API_URL = "http://localhost:8080/api"
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
