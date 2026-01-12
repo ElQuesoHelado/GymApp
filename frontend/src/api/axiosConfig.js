e6460789a383c92002b8b523d35cd8f467ef340a
ElQuesoHelado <josealvacornejo1@gmail.com>
2026-01-12 17:52:29 -0500

    Hardcoded ports

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://backend:8080/api';
+const API_URL =
+  window.location.hostname === "localhost"
+    ? "http://localhost:8080/api"
+    : "http://backend:8080/api";
+
+console.log(window.location.hostname)
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
